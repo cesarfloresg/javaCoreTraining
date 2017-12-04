cadf511aa3737d2aa78a3cb662d04423661b40d0
Oskar <oskar.cv91@gmail.com>
2017-12-05 02:02:37 +0800

    Import static and static block

---
+++ StaticSampleClass.java
@@ -0,0 +1,18 @@
+package com.hcl.saticsample;
+
+public class StaticSampleClass {
+	private String message = "";
+
+	public StaticSampleClass() {
+
+	}
+
+	public StaticSampleClass(String message) {
+		this.message = message;
+
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
